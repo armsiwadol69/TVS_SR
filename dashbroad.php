b1542e1f31fb10c47a0281d123f4a0d8b2f51a1b
SiX <armsiwadol2543@gmail.com>
2022-08-08 16:52:00 +0700

    [ADD] 1st comit | layout | TextOverImage

---
+++ dashbroad.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TVS Station Report : Daily Report</title>
+    <link rel="stylesheet" href="./bootstrap-5.0.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./custom/six.css">
+</head>
+
+<body>
+    <!-- navbart -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">TVS.SR<sup>ALPHA:ยังไม่เสร็จ</sup></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">BigNUTz</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">smolNUTz</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Version : 0.1</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- navbar end -->
+
+    <div class="container-fluid">
+        <div class="row mt-2">
+            <div class="col-lg-4">
+                <div class="card w-100 h-100 shadow-sm">
+                    <div class="card-body text-center inCommingCard">
+                        <h3>INCOMMING</h3>
+                        <div class="row">
+                            <div class="col-6 d-grid gap-4">
+                                <div class="containerImage">        
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 mx-auto d-block mt-5 " alt="">
+                                <h5 class="centeredTextIMG">NUTz</h5>
+                                </div>
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 mx-auto d-block " alt="">
+                            </div>
+                            <div class="col-6 ">
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 imageN" alt="">
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 imageN" alt="">
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 imageN" alt="">
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 imageN" alt="">
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 imageN" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card w-100 h-100 shadow-sm">
+                    <div class="card-body text-center">
+                        <h3>BROADCAST SYSTEM</h3>
+                        <div class="row">
+                            <div class="col-3">
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 imageN" alt="">
+                            </div>
+                            <div class="col-3">
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 imageN" alt="">
+                            </div>
+                            <div class="col-3">
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 imageN" alt="">
+                            </div>
+                            <div class="col-3">
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 imageN" alt="">
+                            </div>
+                            <div class="col-3">
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 imageN" alt="">
+                            </div>
+                            <div class="col-3">
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 imageN" alt="">
+                            </div>
+                            <div class="col-3">
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 imageN" alt="">
+                            </div>
+                            <div class="col-3">
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 imageN" alt="">
+                            </div>
+                            <div class="col-3">
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 imageN" alt="">
+                            </div>
+                            <div class="col-3">
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 imageN" alt="">
+                            </div>
+                            <div class="col-3">
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 imageN" alt="">
+                            </div>
+                            <div class="col-3">
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 imageN" alt="">
+                            </div>
+                            <div class="col-3">
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 imageN" alt="">
+                            </div>
+                            <div class="col-3">
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 imageN" alt="">
+                            </div>
+                            <div class="col-3">
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 imageN" alt="">
+                            </div>
+                            <div class="col-3">
+                                <img src="./image/nutzNRT0.png" class="img-fluid my-1 imageN" alt="">
+                            </div>
+
+                            <hr class=" mt-5">
+
+                            <h3 c>BROADCAST FACILITY</h3>
+                            <div class="row text-center">
+                                <div class="col-3 text-center">
+                                    <h6 class="my-auto">SLA 10 MIN</h6>
+                                </div>
+                                <div class="col-3 ">
+                                    <button type="button" class=" btn btn-secondary w-100">Electrical</button>
+                                </div>
+                                <div class="col-3 ">
+                                    <button type="button" class=" btn btn-secondary w-100">Chiller</button>
+                                </div>
+                                <div class="col-3 text-center">
+                                    <h6 class="">SLA 20 MIN</h6>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="card w-100 h-100 shadow-sm">
+                    <div class="card-body text-center">
+                        <h3>OUTGOING</h3>
+                        <div class="d-grid gap-2 w-100">
+                            <img src="./image/nutzNRT0.png" class="img-fluid mx-auto d-block mt-5 my-1 imageOutgoing"
+                                alt="">
+                            <img src="./image/nutzNRT0.png" class="img-fluid mx-auto d-block my-1 imageOutgoing" alt="">
+                            <img src="./image/nutzNRT0.png" class="img-fluid mx-auto d-block my-1 imageOutgoing" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="card w-100 h-100 shadow-sm">
+                    <div class="card-body text-center">
+                        <h3>TRANSMISSION</h3>
+                        <div class="d-grid gap-2 w-100">
+                            <img src="./image/nutzNRT0.png" class="img-fluid mx-auto d-block my-1 imageTranOut" alt="">
+                            <img src="./image/nutzNRT0.png" class="img-fluid mx-auto d-block my-1 imageTranOut" alt="">
+                            <img src="./image/nutzNRT0.png" class="img-fluid mx-auto d-block my-1 imageTranOut" alt="">
+                            <img src="./image/nutzNRT0.png" class="img-fluid mx-auto d-block my-1 imageTranOut" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="./bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
